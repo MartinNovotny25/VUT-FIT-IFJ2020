f23c973234319181b0e7970d7ea4247bf6ee346e
Martin Novotny <novotny.martin25@gmail.com>
2020-10-25 19:40:32 +0100

    Zacinam prve pravidla LL gramatiky, pridana ERROR kniznica na vypisy

---
+++ error.c
@@ -0,0 +1,63 @@
+//
+// Created by novot on 10/25/2020.
+//
+
+#include "error.h"
+#include "parser.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "symtable.h"
+
+void error_call(int err)
+{
+    int exit_err;
+    switch (err)
+    {
+        case ERR_LEX:
+            fprintf(stderr, "ERR 1 - Lexikalna chyba\n");
+            exit_err = 1;
+            break;
+
+        case ERR_SYN:
+            fprintf(stderr, "ERR 2 - Syntakticka chyba\n");
+            exit_err = 2;
+            break;
+
+        case ERR_SEM_UNDEF:
+            fprintf(stderr, "ERR 3 - Semanticka chyba - nedefinovana funkcia\n");
+            exit_err = 3;
+            break;
+
+        case ERR_SEM_DATATYPE:
+            fprintf(stderr, "ERR 4 - Semanticka chyba - Odvodenie datoveho typu\n");
+            exit_err = 4;
+            break;
+
+        case ERR_SEM_EXCOMPAT:
+            fprintf(stderr, "ERR 5 - Semanticka chyba - Typova kompatibilita\n");
+            exit_err = 5;
+            break;
+
+        case ERR_SEM_RETURN:
+            fprintf(stderr, "ERR 6 - Semanticka chyba - Nespravny pocet parametrov/navratovych hodnot\n");
+            exit_err = 6;
+            break;
+
+        case ERR_SEM_OTHER:
+            fprintf(stderr, "ERR 7 - Semanticka chyba - Ostatne chyby\n");
+            exit_err = 7;
+            break;
+
+        case ERR_SEM_ZERODIV:
+            fprintf(stderr, "ERR 9 - Semanticka chyba - Delenie nulou\n" );
+            exit_err = 9;
+            break;
+
+        case ERR_INTERN:
+            fprintf(stderr, "ERR 99 - Vnutorna chyba prekladaca\n" );
+            exit_err = 99;
+            break;
+    }
+    fprintf(stderr, "UKONCUJEM\n");
+    exit(exit_err);
+}
--- parser.c
+++ parser.c
@@ -2,31 +2,157 @@
 #include <stdio.h>
 
 #include "scanner.h"
+#include "error.h"
 
-int def_func();
+TOKEN token;
+bool params_flag = false;
+
+void rule_eol();
+void def_func();
+void rule_params();
+void rule_params_n();
 
 int main() {
-    TOKEN token;
     token = get_next_token(stdin);
-    int def_return;
+
     //TODO
     /* <prog> -> package main EOL <def_func> EOF */
 
-    /*if(token.type != 27) {fprintf(stderr, "Chybny vstup"); return 2;}
-        else {token = get_next_token(stdin);}*/
-    if (token.type != t_FUNC) { fprintf(stderr, "Chybny vstup"); return 2;}
-        else
-            {
-               def_return = def_func(token);
-            }
+    // eol(y) na zaciatku
+    if (token.type == t_EOL)
+    {
+        token = get_next_token(stdin);
+        rule_eol();
+    }
 
-    if (def_return != 0) { fprintf(stderr, "CHYBA"); return 2;}
-    else {fprintf(stdout, "Dobre"); return 0;}
+    // Test package main
+    if (token.type != t_PACKAGE_MAIN) { error_call(ERR_SYN); }
+        else {token = get_next_token(stdin);}
 
+    // Token EOL
+    if (token.type != t_EOL) { error_call(ERR_SYN);}
+        else {token = get_next_token(stdin);}
+
+    // Neterminal <eol>
+    if (token.type == t_EOL)
+    {
+        token = get_next_token(stdin);
+        rule_eol();
+    }
+    // Neterminal <def_func>
+    if (token.type != t_FUNC) {error_call(ERR_SYN);}
+        else {
+            token = get_next_token(stdin);
+            def_func();
+        }
+
+    return 0;
 }
 
-int def_func(TOKEN data)
+// <def_func>
+void def_func()
 {
+    //printf("som v DEF\n");
+    // terminal ID
+    if (token.type != t_IDENTIFIER) {error_call(ERR_SYN);}
+        else {token = get_next_token(stdin);}
+
+    // token (
+    if (token.type != t_LEFT_BRACKET) {error_call(ERR_SYN);}
+        else {token = get_next_token(stdin);}
+
+    // neterminal <params>
+    if (token.type != t_IDENTIFIER) {error_call(ERR_SYN);}
+        else {
+            //token = get_next_token(stdin);
+            rule_params();
+        }
+    /*token = get_next_token(stdin);
+    token = get_next_token(stdin);
+    token = get_next_token(stdin);*/
 
 
 }
+
+// funkcie pre neterminal <eol>
+void rule_eol()
+{
+    //printf("som v EOL\n");
+    switch (token.type)
+    {
+        case t_EOL:
+            token = get_next_token(stdin);
+            rule_eol();
+            break;
+
+        default:
+            break;
+    }
+}
+
+// funkcia preneterminal <type>
+void rule_type()
+{
+    //printf("som v TYPE\n");
+    switch (token.type)
+    {
+        case t_FLOAT64:
+            token = get_next_token(stdin);
+            break;
+        case t_INT_ID:
+            token = get_next_token(stdin);
+            break;
+        case t_INT_ZERO:
+            token = get_next_token(stdin);
+            break;
+        case t_STRING:
+            token = get_next_token(stdin);
+            break;
+        case t_IDENTIFIER:
+            if (params_flag == true) {error_call(ERR_SYN);}
+            token = get_next_token(stdin);
+            break;
+        default:
+            error_call(ERR_SYN);
+    }
+}
+
+void rule_params()
+{
+    switch (token.type)
+    {
+        case t_IDENTIFIER:
+            token = get_next_token(stdin);
+            rule_type();
+            rule_params_n();
+            break;
+        case t_RIGHT_BRACKET:
+            token = get_next_token(stdin);
+            break;
+    }
+
+}
+
+void rule_params_n()
+{
+    if (token.type != t_COMMA && token.type != t_RIGHT_BRACKET) {error_call(ERR_SYN);}
+        else {token = get_next_token(stdin);}
+
+    switch (token.type)
+    {
+        case t_IDENTIFIER:
+            token = get_next_token(stdin);
+            rule_type();
+            rule_params_n();
+            break;
+        case t_RIGHT_BRACKET:
+            printf("som v right bracket/n");
+            token = get_next_token(stdin);
+            break;
+    }
+
+}
+
+
+
+
