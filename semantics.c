4c1c31c85e63bc7ed577b5134b4f4b6d4030fd25
VladimiraEstragon <andrej.hyros@icloud.com>
2020-11-25 14:52:17 +0100

    vlozenie built-in funkcii

--- semantics.c
+++ semantics.c
@@ -19,7 +19,6 @@ int globalBraceCount = 0;
 //globalna symtable funkcii
 tBSTNodePtrGlobal functions;
 
-//TODO pri kontrole parametrov funkcii treba este overit, ci nemaju parametre rovnake identifikatory...
 //TODO pri kazdom volani erroru uvolnit symtables
 //TODO vlozit so symtable vstavane funkcie
 
@@ -73,6 +72,113 @@ void printFunction(char *id, functionData data) {
 
 
 
+
+
+/*
+ tato funkcia vlozi do globalnej symtable funkcii vstavane funckie
+ func inputs() (string,int)
+ func inputi() (int,int)
+ func inputf() (float64,int)
+ func int2float(i int) (float64)
+ func float2int(f float64) (int)
+ func len(s string) (int)
+ func substr(s string, i int, n int) (string, int)
+ func ord(s string, i int) (int, int)
+ func chr(i int) (string, int)
+ func print ( term1 , term2 , ..., termm )
+ */
+void insertBuiltInFunction() {
+    functionData inputs;
+    inputs.numOfParams = 0;
+    inputs.numOfReturns = 2;
+    inputs.returns[0] = t_STRING_ID;
+    inputs.returns[1] = t_INT_ID;
+    
+    functionData inputi;
+    inputi.numOfParams = 0;
+    inputi.numOfReturns = 2;
+    inputi.returns[0] = t_INT_ID;
+    inputi.returns[1] = t_INT_ID;
+    
+    functionData inputf;
+    inputf.numOfParams = 0;
+    inputf.numOfReturns = 2;
+    inputf.returns[0] = t_FLOAT64;
+    inputf.returns[1] = t_INT_ID;
+    
+    functionData int2float;
+    int2float.numOfParams = 1;
+    int2float.numOfReturns = 1;
+    int2float.returns[0] = t_FLOAT64;
+    int2float.paramsType[0] = t_INT_ID;
+    int2float.params[0] = "i";
+    
+    functionData float2int;
+    float2int.numOfParams = 1;
+    float2int.numOfReturns = 1;
+    float2int.returns[0] = t_INT_ID;
+    float2int.paramsType[0] = t_FLOAT64;
+    float2int.params[0] = "f";
+    
+    functionData len;
+    len.numOfParams = 1;
+    len.numOfReturns = 1;
+    len.returns[0] = t_INT_ID;
+    len.paramsType[0] = t_STRING_ID;
+    len.params[0] = "s";
+    
+    functionData substr;
+    substr.numOfParams = 3;
+    substr.numOfReturns =2;
+    substr.returns[0] = t_STRING_ID;
+    substr.returns[1] = t_INT_ID;
+    substr.paramsType[0] = t_STRING_ID;
+    substr.paramsType[1] = t_INT_ID;
+    substr.paramsType[2] = t_INT_ID;
+    substr.params[0] = "s";
+    substr.params[1] = "i";
+    substr.params[2] = "n";
+    
+    functionData ord;
+    ord.numOfParams = 2;
+    ord.numOfReturns =21;
+    ord.returns[0] = t_INT_ID;
+    ord.returns[1] = t_INT_ID;
+    ord.paramsType[0] = t_STRING_ID;
+    ord.paramsType[1] = t_INT_ID;
+    ord.params[0] = "s";
+    ord.params[0] = "i";
+    
+    functionData chr;
+    chr.numOfParams = 1;
+    chr.numOfReturns = 2;
+    chr.returns[0] = t_STRING_ID;
+    chr.returns[1] = t_INT_ID;
+    chr.paramsType[0] = t_INT_ID;
+    chr.params[0] = "i";
+    
+    functionData print;
+    print.numOfParams = MAXPARAMS;
+    print.numOfReturns = 0;
+    
+    BSTInsertGlobal(&functions, "inputs", inputs);
+    BSTInsertGlobal(&functions, "inputi", inputi);
+    BSTInsertGlobal(&functions, "inputf", inputf);
+    BSTInsertGlobal(&functions, "int2float", int2float);
+    BSTInsertGlobal(&functions, "float2int", float2int);
+    BSTInsertGlobal(&functions, "len", len);
+    BSTInsertGlobal(&functions, "substr", substr);
+    BSTInsertGlobal(&functions, "ord", ord);
+    BSTInsertGlobal(&functions, "chr", chr);
+    BSTInsertGlobal(&functions, "print", print);
+}
+
+/*
+ tato funkcia kontroluje nazvy parametrov vo funkcii, nemozu sa volat rovnako
+ TODO parametre sa budu musiet vlozit aj do lokalnej symtable
+ @param data struktura obsahujuca pocet a typy parametrov
+ @param L ukazatel na zoznam tokenov, potrebny pre potreby uvolnenia
+ */
 void paramsRedefinitionCheck(functionData data, TDLList *L) {
     printf("pocet parametrov ktore kontrolujem: %d\n", data.numOfParams);
     printf("sem: som v paramsRedefinitionCheck\n");
@@ -81,9 +187,7 @@ void paramsRedefinitionCheck(functionData data, TDLList *L) {
     }
     
     for (int i = 0; i < data.numOfParams; i++) {
-        //printf("1st int, %d\n", i);
         for (int j = 0; j < data.numOfParams; j++) {
-            //printf("2st int, %d\n", j);
             //porovname parameter sameho so sebou, tento krok treba preskocit
             if (i == j) {
                 continue;
@@ -255,7 +359,7 @@ void goThroughList(TDLList *L) {
     
     //inicializacia tabulky funkcii
     BSTInitGlobal(&functions);
-    
+    insertBuiltInFunction();
     TDLLFirst(L);
     printf("Semantika: Prechadzam zoznamom tokenov...\n\n");
     int i = 0, j = 0;      //pre potreby vypiskov
